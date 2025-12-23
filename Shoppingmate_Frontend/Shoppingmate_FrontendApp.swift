24174b18711347c0dd0deda6ecce2820fceeef98
chaewon-45 <22300392@handong.ac.kr>
2025-12-23 11:58:34 +0900

    initial commit

---
+++ Shoppingmate_Frontend/Shoppingmate_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoppingmate_FrontendApp.swift
+//  Shoppingmate_Frontend
+//
+//  Created by 손채원 on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Shoppingmate_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
